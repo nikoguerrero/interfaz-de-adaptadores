349d6b98933c6847c7143e388c4c7aae7339a2b5
nikoguerrero <nclguerrero@gmail.com>
2021-10-20 19:27:47 -0300

    reading yaml text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+// const yaml = require('js-yaml');
+// const fs   = require('fs');
+
+import { load } from 'js-yaml'
+import { fs } from 'fs';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+  const [output, setOutput] = useState('');
+  useEffect(() => {
+    const value = localStorage.getItem('text');
+    setText(value);
+    log({target: {value: text}});
+  }, []);
+  const log = ({ target: {value: text}}) => Promise.resolve(text)
+    .then(load)
+    .then( t => JSON.stringify(t, null, 2))
+    .then(setOutput)
+    .then(() => localStorage.setItem('text', text));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <textarea
+        rows="20"
+        onChange={log}
+        defaultValue={text}
+        style={{width: '100%'}}>
+      </textarea>
+      <pre>
+        <code>
+          {output}
+        </code>
+      </pre>
     </div>
   );
 }
