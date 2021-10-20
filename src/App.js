6da02d3265cbbde1d250cbbca9f39547c1506d06
Niko Guerrero <nclguerrero@gmail.com>
2021-10-20 20:04:27 -0300

    Fetching yml file

--- src/App.js
+++ src/App.js
@@ -9,31 +9,21 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [text, setText] = useState('');
-  const [output, setOutput] = useState('');
+  // const [output, setOutput] = useState('');
+
   useEffect(() => {
-    const value = localStorage.getItem('text');
-    setText(value);
-    log({target: {value: text}});
+    fetch('./data/config/keyFinder.yml')
+      .then((response) => response.text())
+      .then((yamlText) => {
+        const yamlObject = load(yamlText);
+        console.log(yamlObject);
+        setText(JSON.stringify(yamlObject));
+      }).catch((err) => console.log('failed to load yaml file', err)); 
   }, []);
-  const log = ({ target: {value: text}}) => Promise.resolve(text)
-    .then(load)
-    .then( t => JSON.stringify(t, null, 2))
-    .then(setOutput)
-    .then(() => localStorage.setItem('text', text));
 
   return (
-    <div>
-      <textarea
-        rows="20"
-        onChange={log}
-        defaultValue={text}
-        style={{width: '100%'}}>
-      </textarea>
-      <pre>
-        <code>
-          {output}
-        </code>
-      </pre>
+    <div style={{width: '100%'}}>
+      {text}
     </div>
   );
 }
