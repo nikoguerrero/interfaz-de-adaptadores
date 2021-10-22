0c5f0d6b232eefbc64f7fc9b3117c8369eea3ac7
GabbyHolics <karenthgabriela@gmail.com>
2021-10-22 20:48:25 -0300

    Interface v.1

--- src/components/Orchestration.jsx
+++ src/components/Orchestration.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Orchestration = () => {
     return (
-        <div>
-            
+      <>
+        <h2 className="h1 text-center">Orchestration</h2>
+        <div className="row">
+          <div className="d-grid  col-7  mx-auto ">
+            <button type="button" className="btn btn-secondary text-white  mt-3 "> Plugin Info</button>
+          </div>
         </div>
-    )
+      </>
+    );
 }
 
 export default Orchestration
