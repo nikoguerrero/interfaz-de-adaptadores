9e54f3aa510fece2d277992e900690a6008a2f9f
GabbyHolics <karenthgabriela@gmail.com>
2021-10-22 14:46:41 -0300

    added form input and modified classes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,31 +1,100 @@
 import React from 'react'
 
 const Form = () => {
-    return (
-      <div>
-        <h2 className="h1 text-center">Form</h2>
-        <div class="row g-3">
-  <div class="col-sm-6">
-  <div class="form-floating mb-3">
-  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-  <label for="floatingInput">Email address</label>
-</div>
-  </div>
-  <div class="col-sm-6">
-  <div class="form-floating mb-3">
-  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-  <label for="floatingInput">Email address</label>
-</div>
-  </div>
-  <div class="col-sm">
-    <div class="form-outline">
-      <input type="text" id="form10Example3" class="form-control" />
-      <label class="form-label" for="form10Example3">Name</label>
-    </div>
-  </div>
-</div>
+  return (
+    <div>
+      <h2 className="h1 text-center">Form</h2>
+      <h5 className="text-primary">Plugin Configuration</h5>
+      <div className="row g-2">
+        <div className="col-sm-5">
+          <div className="form-floating mb-3 ">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="name@example.com"
+            />
+            <label>Email address</label>
+          </div>
+        </div>
+        <div className="col-sm-5">
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="name@example.com"
+            />
+            <label>Email address</label>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="name@example.com"
+            />
+            <label>Email address</label>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="name@example.com"
+            />
+            <label>Email address</label>
+          </div>
+        </div>
       </div>
-    );
-}
+      <h5 className="text-primary">Config</h5>
+        <div className="row g-2">
+          <div className="col-sm-6">
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="name@example.com"
+              />
+              <label>Email address</label>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="name@example.com"
+              />
+              <label>Email address</label>
+            </div>
+          </div>
+        </div>
+        <h5 className="text-primary">Fields</h5>
+        <div className="row g-2">
+          <div className="col-sm-6">
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="name@example.com"
+              />
+              <label>Email address</label>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="name@example.com"
+              />
+              <label>Email address</label>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+};
 
 export default Form
