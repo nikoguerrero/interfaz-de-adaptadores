2f16e55691eb35da38db1c1dd4c168e0b4f5ac1c
GabbyHolics <karenthgabriela@gmail.com>
2021-10-24 12:36:13 -0300

    change vh-100

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,10 +24,10 @@ const Main = () => {
       <div className="container-fluid vh-100">
         <Header />
         <div className="row">
-          <div className="col-3 vh-100  bg-secondary bg-opacity-10 ackground-plugins">
+          <div className="col-3   bg-secondary bg-opacity-10 ackground-plugins">
             <Plugins showPluginForm={showPluginForm} setShow={setShow}/>
           </div>
-          <div className="col-6 vh-100 ">
+          <div className="col-6  ">
             <Adapter adapterArray={adapterArray} show={show}/>
           </div>
           <div className="col-3  bg-secondary bg-opacity-25">
