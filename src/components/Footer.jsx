6a35de40de3506d0508bec1f5765d0859efe0858
MartaSVinet <isabelle.vinet@gmail.com>
2021-10-24 19:46:59 -0300

    add localStorage saving and clear

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div class="p-3 mb-1 bg-dark bg-gradient"></div>
+        <div class="p-3 mb-1 bg-dark bg-gradient d-flex"></div>
     )
 };
 
